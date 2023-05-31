5ddc8c92f1d494c4a24d3e73abbee1c3c9558839
JosePalenciaa <100240798+JosePalenciaa@users.noreply.github.com>
2023-06-01 11:05:13 +1200

    Instructions + Sides + Difficulty

---
+++ 04_Pythagoras/01_instructions.py
@@ -0,0 +1,37 @@
+def instructions():
+    print("---------------------")
+    print("**** How to Play ****")
+    print("---------------------")
+    print()
+    print("Your goal is to calculate the missing side using the 'Pythagoras Theorem'..."
+          "\n• Formulas:\n\tHypotenuse | a² + b² = c²\n\tOpposite | c² - a² = b²\n\tAdjacent | c² - b² = a²"
+          "\n\tHypotenuse = c | Opposite / Adjacent = b / a"
+          "\n\n• First select a difficulty."
+          "\n• You are given 3 attempts. "
+          "\n• Solve the missing side. "
+          "\n• Round your answer to whole integers."
+          "\n• Can you win?")
+    print()
+
+
+def yes_no_why(question):
+    while True:
+        response = input(question).lower()
+
+        for item in yesno_list:
+            if response == item[0] or response == item:
+                return item
+
+        print("Please enter a valid response (yes / no / why)")
+        print()
+
+
+yesno_list = ["yes", "no", "why"]
+
+display_instructions = yes_no_why("Do you want to see Instructions? ")
+
+if display_instructions == "yes":
+    instructions()
+
+elif display_instructions == "why":
+    print("Because I said so!!!")
---
+++ 04_Pythagoras/03_difficulty.py
@@ -0,0 +1,27 @@
+def difficult(question):
+    while True:
+        response = input(question).lower()
+
+        for item in diff_list:
+            if response == item[0] or response == item:
+                return item
+
+        if question == "easy":
+            print("Easy Mode")
+
+        elif question == "moderate":
+            print("Moderate Mode")
+
+        elif question == "hard":
+            print("Hard Mode")
+
+        elif question == "xxx":
+            break
+
+        print("Please enter a valid response (yes / no / why)")
+        print()
+
+
+diff_list = ["easy", "moderate", "hard", "xxx"]
+
+ask_diff = difficult("What difficulty would you like? ")
