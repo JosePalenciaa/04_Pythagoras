8ba502c83b6c1120def9026e292543cc755f2cdb
JosePalenciaa <100240798+JosePalenciaa@users.noreply.github.com>
2023-07-19 11:58:48 +1200

    Add files via upload

--- 04_Pythagoras/11_integer_float.py
+++ 04_Pythagoras/11_integer_float.py
@@ -23,6 +23,21 @@ def number_checker(question, allow_floats="yes"):
 
         try:
             if allow_floats == "yes":
-                response = float(question)
+                response = float(response)
+
+            elif allow_floats == "no":
+                response = int(response)
+        except ValueError:
+            print("Please enter a valid number")
+            continue
 
         return response
+
+# Testing...
+
+
+user_integer = number_checker("Please input an integer: ", allow_floats="no")
+
+user_number = number_checker("Please input a number: ", allow_floats="yes")
+
+print(f"\nInteger: {user_integer}\t\t\tNumber: {user_number}")
